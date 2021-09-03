a6448a5b24dc91a69b116414b10615e9cf7f7660
taxilng <769967440@qq.com>
2021-09-03 15:56:34 +0800

    style: 样式修改；

--- src/views/home/tips.vue
+++ src/views/home/tips.vue
@@ -1,7 +1,7 @@
 <template>
     <el-dialog title="提示" :visible.sync="addDialog" width="300px" :before-close="handleClose">
         <div>
-            因为本人阿里云于2021年11月6日到期。所以我重新找了个免费的服务器用部署。大家可以点击 <a href="https://fundsforweb.netlify.app" target="_blank">新基金网页版</a> 来访问。因为是国外服务器，速度较慢。<br /><br />大家的基金数据可以通过设置中的账号同步，导出配置文件，导出文本三种方式来同步到新的网站上。
+            因为本人阿里云于2021年11月6日到期。所以我重新找了个免费的服务器用部署。大家可以点击 <a href="https://fundsforweb.netlify.app" target="_blank" style="color: red;">新基金网页版</a> 来访问。因为是国外服务器，速度较慢。<br /><br />大家的基金数据可以通过设置中的账号同步，导出配置文件，导出文本三种方式来同步到新的网站上。
         </div>
         <span slot="footer" class="dialog-footer">
             <el-button size="small" @click="day = 7;handleClose();">7天内不再提示</el-button>
