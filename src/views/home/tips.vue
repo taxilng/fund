fb10d7286221558ec9cb0c23c99d9619a48e3a72
taxilng <769967440@qq.com>
2021-09-08 11:50:42 +0800

    feat: 追加新的地址；

--- src/views/home/tips.vue
+++ src/views/home/tips.vue
@@ -2,6 +2,7 @@
     <el-dialog title="提示" :visible.sync="addDialog" width="300px" :before-close="handleClose">
         <div>
             因为本人阿里云于2021年11月6日到期。所以我重新找了个免费的服务器用部署。大家可以点击 <a href="https://fundsforweb.netlify.app" target="_blank" style="color: red;">新基金网页版</a> 来访问。因为是国外服务器，速度较慢。<br /><br />大家的基金数据可以通过设置中的账号同步，导出配置文件，导出文本三种方式来同步到新的网站上。
+<br/><br />补充一个新的链接，布置在阿里云上，速度较快<a href="http://120.26.91.101:8088/" target="_blank" style="color: red;">新基金网页版2</a>
         </div>
         <span slot="footer" class="dialog-footer">
             <el-button size="small" @click="day = 7;handleClose();">7天内不再提示</el-button>
